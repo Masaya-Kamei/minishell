adb53ffd10c7b0f9d57ca3830856326db9bc2968
mkamei <mkamei@student.42tokyo.jp>
2021-05-29 19:05:06 +0900

    Edit minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,23 @@
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 09:39:27 by mkamei            #+#    #+#             */
-/*   Updated: 2021/05/28 09:40:28 by mkamei           ###   ########.fr       */
+/*   Updated: 2021/05/29 18:40:10 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
 # include "../libft/libft.h"
+# include <stdio.h>
+
+# define BUFFER_SIZE 10
+# define SUCCESS 0
+# define ERROR 1
+# define MULTIPLE_LINES 2
+
+int		free_and_return(void *p, int status);
+void	free_double_pointer(char **strs);
+int		parse_line(char *line, char ***argv);
 
 #endif
