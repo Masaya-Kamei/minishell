aa4de173cc5f770e2f4ac8a53608f9121e9f90cc
Masaya Kamei <km9smc6@gmail.com>
2021-05-31 17:37:09 +0900

    Add debug.c utils.c

--- srcs/debug.c
+++ srcs/debug.c
@@ -1,32 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   debug.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/29 18:28:52 by mkamei            #+#    #+#             */
-/*   Updated: 2021/05/30 17:51:44 by mkamei           ###   ########.fr       */
+/*   Created: 2021/05/30 17:55:37 by mkamei            #+#    #+#             */
+/*   Updated: 2021/05/31 13:57:21 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	free_double_pointer(char **strs)
+void	print_line_and_word_start_array(char *line, int *word_start_array)
 {
 	int		i;
+	int		j;
 
 	i = 0;
-	while (strs[i] != NULL)
+	j = 0;
+	while (line[i] != '\0')
 	{
-		free(strs[i]);
+		if (i == word_start_array[j])
+		{
+			printf("#");
+			j++;
+		}
+		else if (line[i] == '\n')
+			printf("\n");
+		else
+			printf(" ");
 		i++;
 	}
-	free(strs);
+	printf("%s\n", line);
 }
 
-int	free_and_return(void *p, int status)
+void	print_argv(char **argv)
 {
-	free(p);
-	return (status);
+	int	i;
+
+	i = 0;
+	while (argv[i] != NULL)
+	{
+		printf("argv[%2d]	:%s\n", i, argv[i]);
+		i++;
+	}
 }
