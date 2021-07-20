e1b0cef7f3c0a1111a26c411606af5f5d93f4a0b
Masaya Kamei <km9smc6@gmail.com>
2021-07-20 19:18:08 +0900

    Norm対応、エラー出力関数の変更など

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 15:30:07 by mkamei            #+#    #+#             */
-/*   Updated: 2021/07/15 17:51:44 by mkamei           ###   ########.fr       */
+/*   Updated: 2021/07/20 19:05:27 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	handler(int signum)
 	g_received_signal = signum;
 }
 
-int	redisplay_prompt(void)
+static int	redisplay_prompt(void)
 {
 	if (g_received_signal == SIGINT)
 	{
@@ -36,43 +36,43 @@ int	redisplay_prompt(void)
 	return (0);
 }
 
+static void	exit_by_eof(t_list *vars_list[3])
+{
+	clear_vars_list(vars_list);
+	printf("\033[1A\033[11C");
+	rl_redisplay();
+	write(2, "exit\n", 5);
+	exit(0);
+}
+
 static void	loop_minishell(t_list *vars_list[3])
 {
-	char	*line;
-	t_token	*tokens;
-	int		token_num;
-	int		status;
+	char		*line;
+	t_token		*tokens;
+	int			token_num;
+	t_status	status;
 
-	while (1)
+	status = SUCCESS;
+	while (status == SUCCESS)
 	{
 		g_received_signal = 0;
 		line = readline("minishell$ ");
 		if (line == NULL)
-		{
-			clear_vars_list(vars_list);
-			printf("\033[1A\033[11C");
-			rl_redisplay();
-			write(2, "exit\n", 5);
-			exit(0);
-		}
+			exit_by_eof(vars_list);
 		if (line[0] != '\0')
 			add_history(line);
 		status = lex_line(line, &tokens, &token_num);
 		free(line);
-		if (status == ERR_MALLOC)
-		{
-			write_shell_err(NULL, ERR_MALLOC, ORIGINAL);
-			set_exit_status(vars_list[SPECIAL], 1);
-			continue ;
-		}
-		else if (token_num == 0)
-		{
-			free_tokens(tokens);
-			continue ;
-		}
-		print_tokens(tokens, vars_list);
-		test_vars_list(vars_list);
-		// status = process_pipeline(tokens, 0, token_num - 1);
+		if (status == E_MALLOC)
+			break ;
+		status = start_process(tokens, 0, token_num - 1, vars_list);
+		free_tokens(tokens);
+	}
+	clear_vars_list(vars_list);
+	exit(get_exit_status_with_errout(NULL, status, P_SHELL));
+}
+		// print_tokens(tokens, vars_list);
+		// test_vars_list(vars_list);
 		// g_received_signal = 0;
 		// execve_sleep();
 		// if (g_received_signal == SIGINT)
@@ -85,13 +85,6 @@ static void	loop_minishell(t_list *vars_list[3])
 		// 	g_received_signal = 0;
 		// 	printf("Quit: 3\n");
 		// }
-		free_tokens(tokens);
-		// if (status == ERR_MALLOC)
-		// 	exit(1);
-		// else if (status != SUCCESS)
-		// 	write_err_msg(status);
-	}
-}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -100,23 +93,23 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)**argv;
 	if (signal(SIGINT, handler) == SIG_ERR)
-		exit(write_shell_err(NULL, errno, ERRNO));
+		exit(get_exit_status_with_errout(NULL, E_SIGNAL, P_SHELL));
 	if (signal(SIGQUIT, handler) == SIG_ERR)
-		exit(write_shell_err(NULL, errno, ERRNO));
+		exit(get_exit_status_with_errout(NULL, E_SIGNAL, P_SHELL));
 	rl_signal_event_hook = &redisplay_prompt;
 	vars_list[SHELL] = NULL;
 	vars_list[SPECIAL] = lstnew_with_strdup("?=0  ");
 	if (vars_list[SPECIAL] == NULL)
-		exit(write_shell_err(NULL, ERR_MALLOC, ORIGINAL));
+		exit(get_exit_status_with_errout(NULL, E_MALLOC, P_SHELL));
 	((char *)vars_list[SPECIAL]->content)[3] = '\0';
 	vars_list[ENV] = create_env_list(envp);
 	if (vars_list[ENV] == NULL
-		|| set_oldpwd_var(vars_list, 1) == ERR_MALLOC
-		|| set_pwd_var(vars_list, 1) == ERR_MALLOC
-		|| countup_shlvl_env(&vars_list[ENV]) == ERR_MALLOC)
+		|| set_oldpwd_var(vars_list, 1) == E_MALLOC
+		|| set_pwd_var(vars_list, 1) == E_MALLOC
+		|| countup_shlvl_env(&vars_list[ENV]) == E_MALLOC)
 	{
 		clear_vars_list(vars_list);
-		exit(write_shell_err(NULL, ERR_MALLOC, ORIGINAL));
+		exit(get_exit_status_with_errout(NULL, E_MALLOC, P_SHELL));
 	}
 	loop_minishell(vars_list);
 	return (0);
