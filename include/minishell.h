fc70c1009f0766479ee72ac3439c54bae1e9e3b7
mkamei <mkamei@student.42tokyo.jp>
2021-08-01 17:48:03 +0900

    Makefile,headerを修正

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: keguchi <keguchi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 09:39:27 by mkamei            #+#    #+#             */
-/*   Updated: 2021/07/30 16:18:40 by keguchi          ###   ########.fr       */
+/*   Updated: 2021/08/01 17:42:05 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,28 @@ typedef enum e_str_type{
 typedef enum e_status
 {
 	SUCCESS 			= 0,
-	E_INVALID_OP 		= 1,
-	E_NOSET_VAR 		= 2,
-	E_NUM_ARG_REQ 		= 3,
-	E_TOO_MANY_ARG 		= 4,
-	E_INVALID_ID 		= 5,
-	E_INVALID_OP_ARG	= 6,
-	E_SYNTAX 			= 7,
-	E_SIGNAL			= 8,
-	E_MALLOC			= 9,
-	E_DUP_CLOSE			= 10,
-	E_CHDIR				= 11,
-	E_FORK				= 12,
-	E_OPEN				= 13,
-	E_UNLINK			= 14,
-	E_EXECVE			= 15,
-	E_EXTERNAL			= 16,
-	E_CHILD				= 17,
-	E_NULL_FILE			= 18,
-	E_EMPTY_FILE		= 19
+	E_SYSTEM			= 1,
+	E_INVALID_OP 		= 2,
+	E_NOSET_VAR 		= 3,
+	E_NUM_ARG_REQ 		= 4,
+	E_TOO_MANY_ARG 		= 5,
+	E_INVALID_ID 		= 6,
+	E_INVALID_OP_ARG	= 7,
+	E_SYNTAX 			= 8,
+	E_NOCOMMAND			= 9,
+	E_AMBIGUOUS			= 10,
+	E_OPEN				= 11
+	// E_SIGNAL			= 8,
+	// E_MALLOC			= 9,
+	// E_DUP_CLOSE			= 10,
+	// E_CHDIR				= 11,
+	// E_FORK				= 12,
+	// E_OPEN				= 13,
+	// E_UNLINK			= 14,
+	// E_EXECVE			= 15,
+	// E_CHILD				= 17,
+	// E_NULL_FILE			= 18,
+	// E_EMPTY_FILE		= 19
 }			t_status;
 
 typedef enum e_err_place
@@ -91,9 +94,10 @@ typedef struct s_token
 	t_token_type	type;
 }				t_token;
 
-typedef int	t_exit_status;
+typedef int				t_exit_status;
+typedef t_exit_status	(*t_builtin_func)(char **, t_list **);
 
-int			g_received_signal;
+int						g_received_signal;
 
 t_status		lex_line(char *line, t_token **tokens, int *token_num);
 t_status		expand_word_token(char **word, t_list *vars_list[3]);
@@ -113,16 +117,20 @@ t_list			*lstnew_with_strdup(char *str);
 t_list			*get_target_list(t_list *any_list, char *var, int var_name_len);
 t_status		add_new_var(t_list **any_list, char *var);
 char			*get_var(t_list *vars_list[3], char *var_name);
-t_status		set_var(t_list *vars_list[3], char *var, int var_type);
-void			delete_var(t_list *vars_list[3], char *var_name, int var_type);
+t_status		set_var(t_list *vars_list[3], char *var, t_vars_type var_type);
+void			delete_var(
+					t_list *vars_list[3], char *var_name, t_vars_type var_type);
 char			*get_current_absolute_path(void);
-t_status		set_pwd_var(t_list *vars_list[3], int init);
-t_status		set_oldpwd_var(t_list *vars_list[3], int init);
-t_status		set_exit_status(t_list *special_list, int exit_status);
+t_status		set_pwd_var(t_list *vars_list[3], t_bool init);
+t_status		set_oldpwd_var(t_list *vars_list[3], t_bool init);
+void			set_exit_status(
+					t_list *special_list, t_exit_status exit_status);
 t_status		countup_shlvl_env(t_list **env_list);
 t_exit_status	get_exit_status_with_errout(
 					char *word, t_status status, t_err_place err_place);
-void			free_double_pointer(void **strs);
+void			set_exit_status_with_errout(
+					char *word, t_status status, t_list *vars_list[3]);
+void			free_double_pointer(void **p);
 void			free_tokens(t_token *tokens);
 t_status		free_and_return(void *p, t_status status);
 void			free_and_fill_null(char **p);
@@ -133,9 +141,14 @@ void			execve_sleep(void);
 void			test_vars_list(t_list *vars_list[3]);
 t_status		debug_process_command(
 					t_token *tokens, int start, int end, t_list *vars_list[3]);
-t_status		process_command(t_token *tokens, int start_index, int end_index, t_list *vars_list[3]);
-t_status		get_command_argv(t_token *tokens, char ***command, int start_index, int end_index);
-t_status		redirect_list(t_token *tokens, int start_index, int end_index, int **save_fd);
+t_status		process_command(
+					t_token *tokens, int start, int end, t_list *vars_list[3]);
+t_status		process_redirect(t_token *tokens, int start, int **save_fd);
 char			*get_command_path(char *cmd);
+t_status		strjoin_to_cmd_str(t_token *tokens,
+					int word_index, char **cmd_str, t_list *vars_list[3]);
+t_status		malloc_to_save_fd(
+					t_token *tokens, int start, int end, int ***save_fd);
+t_status		restore_fd(int **save_fd);
 
 #endif
