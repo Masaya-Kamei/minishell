94b563fb8bbce3873e3fcc1634b828c27db46a7e
mkamei <mkamei@student.42tokyo.jp>
2021-08-10 18:58:37 +0900

    pwdのあとにpwd_varにセット

--- srcs/var/var_set_any.c
+++ srcs/var/var_set_any.c
@@ -6,49 +6,40 @@
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 14:03:48 by mkamei            #+#    #+#             */
-/*   Updated: 2021/08/10 15:47:55 by mkamei           ###   ########.fr       */
+/*   Updated: 2021/08/10 18:54:54 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_status	set_pwd_var(t_list *vars_list[3],
-	t_vars_type var_type, t_place place, char *cd_target_dir)
+static t_status	set_pwd_var(t_data *d, t_place place)
 {
 	char		*var;
-	char		*value;
 	t_status	status;
+	t_vars_type	var_type;
 
-	value = getcwd(NULL, 0);
-	if (value == NULL)
-	{
-		write_err(NULL, E_GETCWD, 1, place);
-		value = get_var(vars_list, "PWD");
-		if (value == NULL && cd_target_dir == NULL)
-			return (SUCCESS);
-		value = create_full_path(value, cd_target_dir);
-		if (value == NULL)
-			return (E_SYSTEM);
-	}
-	var = ft_strjoin("PWD=", value);
-	free(value);
+	if (place == P_SHELL)
+		var_type = ENV;
+	else
+		var_type = SHELL;
+	var = ft_strjoin("PWD=", d->pwd);
 	if (var == NULL)
 		return (E_SYSTEM);
-	status = set_var(vars_list, var, var_type);
+	status = set_var(d->vars_list, var, var_type);
 	free(var);
 	if (status == E_SYSTEM)
 		return (E_SYSTEM);
 	return (SUCCESS);
 }
 
-static t_status	set_oldpwd_var(t_list *vars_list[3], t_vars_type var_type)
+static t_status	set_oldpwd_var(t_list *vars_list[3], t_place place)
 {
 	t_list		*target_list;
 	char		*var;
 	char		*value;
 	t_status	status;
 
-	if (var_type == ENV)
+	if (place == P_SHELL)
 	{
 		target_list = get_target_list(vars_list[ENV], "OLDPWD", 6);
 		if (target_list == NULL)
@@ -72,24 +63,25 @@ static t_status	set_oldpwd_var(t_list *vars_list[3], t_vars_type var_type)
 
 t_status	set_pwd(t_data *d, t_place place, char *cd_target_dir)
 {
-	t_vars_type	var_type;
-	char		*pwd_value;
+	char	*new_pwd;
 
-	if (place == P_SHELL)
-		var_type = ENV;
-	else
-		var_type = SHELL;
-	if (set_oldpwd_var(d->vars_list, var_type) == E_SYSTEM)
-		return (E_SYSTEM);
-	if (set_pwd_var(d->vars_list, var_type, place, cd_target_dir) == E_SYSTEM)
-		return (E_SYSTEM);
-	pwd_value = get_var(d->vars_list, "PWD");
-	if (pwd_value == NULL)
-		return (SUCCESS);
+	new_pwd = getcwd(NULL, 0);
+	if (new_pwd == NULL)
+	{
+		write_err(NULL, E_GETCWD, 1, place);
+		if (d->pwd == NULL && cd_target_dir == NULL)
+			return (SUCCESS);
+		new_pwd = create_full_path(d->pwd, cd_target_dir);
+		if (new_pwd == NULL)
+			return (E_SYSTEM);
+	}
 	free(d->pwd);
-	d->pwd = ft_strdup(pwd_value);
-	if (d->pwd == NULL)
+	d->pwd = new_pwd;
+	if (set_oldpwd_var(d->vars_list, place) == E_SYSTEM
+		|| set_pwd_var(d, place) == E_SYSTEM)
+	{
 		return (E_SYSTEM);
+	}
 	return (SUCCESS);
 }
 
