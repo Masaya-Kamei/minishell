9c22c0bd8c593eeee8b433b19f0b7255e90f7344
keguchi-42 <keigo0630x@gmail.com>
2021-08-27 21:27:07 +0900

    lstadd_frontに変更。INT_MAXのエラー処理を改良。red_fdをfreeしてからreturnに変更。pipe失敗時、E_SYSTEMで返す。norm対応。

--- srcs/process_redirect.c
+++ srcs/process_redirect.c
@@ -6,7 +6,7 @@
 /*   By: keguchi <keguchi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 09:25:38 by keguchi           #+#    #+#             */
-/*   Updated: 2021/08/27 19:05:58 by keguchi          ###   ########.fr       */
+/*   Updated: 2021/08/27 21:24:47 by keguchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static long long int	get_redirect_num(char *str)
 {
 	int				i;
-	unsigned long	nbr;
+	long long int	nbr;
 
 	i = 0;
 	nbr = 0;
@@ -23,9 +23,9 @@ static long long int	get_redirect_num(char *str)
 		return (STDIN_FILENO);
 	else if (ft_strncmp(str, ">", 2) == 0 || ft_strncmp(str, ">>", 3) == 0)
 		return (STDOUT_FILENO);
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9' && nbr < INT_MAX)
 		nbr = (nbr * 10) + str[i++] - '0';
-	if (nbr > UINT_MAX)
+	if (nbr > INT_MAX)
 		return (-1);
 	return (nbr);
 }
@@ -36,26 +36,26 @@ static t_status	set_redirect_and_save_fd(t_list **save_fd,
 	t_list	*new;
 	int		pipe_fd[2];
 
-	red_fd[1] = dup(red_fd[0]);
-	if (red_fd[1] == -1 && (red_fd[0] > 10496))
+	if (red_fd[1] == -1 && red_fd[0] > 10496)
 		return (E_OVER_LIMIT);
-	if (red_fd[1] == -1 && (red_fd[0] > INT_MAX || red_fd[0] < 0))
+	if (red_fd[1] == -1 && red_fd[0] < 0)
 		return (E_OVER_FD);
 	if (red_fd[1] == -1)
 		return (SUCCESS);
 	new = ft_lstnew(red_fd);
 	if (!new)
 		return (E_SYSTEM);
-	ft_lstadd_back(save_fd, new);
+	ft_lstadd_front(save_fd, new);
 	if (fd == -1)
 	{
-		pipe(pipe_fd);
+		if (pipe(pipe_fd) < 0)
+			return (E_SYSTEM);
 		write(pipe_fd[1], heredoc, ft_strlen(heredoc));
-		if (close(pipe_fd[1]) == -1 || dup2(pipe_fd[0], red_fd[0]) == -1
-			|| close(pipe_fd[0]) == -1)
+		fd = pipe_fd[0];
+		if (close(pipe_fd[1]) == -1)
 			return (E_SYSTEM);
 	}
-	else if (dup2(fd, red_fd[0]) == -1 || close(fd) == -1)
+	if (dup2(fd, red_fd[0]) == -1 || close(fd) == -1)
 		return (E_SYSTEM);
 	return (SUCCESS);
 }
@@ -74,7 +74,6 @@ static t_status	open_and_redirect_file(t_token token,
 	red_fd = malloc(sizeof(long long int) * 2);
 	if (!red_fd)
 		return (E_SYSTEM);
-	red_fd[0] = get_redirect_num(token.str);
 	if (token.type == '>')
 		fd = open(expanded_str, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	else if (token.type == '<')
@@ -85,10 +84,13 @@ static t_status	open_and_redirect_file(t_token token,
 		return (E_OPEN);
 	if (token.type == 'L')
 		fd = -1;
+	red_fd[0] = get_redirect_num(token.str);
+	red_fd[1] = dup(red_fd[0]);
 	// tokensに新しいメンバを追加した場合、ここで出力文を入れ替える。
 	// if (token.expand_flag)
 	// 	return (set_redirect_and_save_fd(save_fd, red_fd, fd, tokens[i + 1].str));
-	return (set_redirect_and_save_fd(save_fd, red_fd, fd, expanded_str));
+	return (free_and_return(red_fd,
+			set_redirect_and_save_fd(save_fd, red_fd, fd, expanded_str)));
 }
 
 static void	redirect_replaced_null(char *str)
